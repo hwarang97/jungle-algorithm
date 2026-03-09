534fd9159cb4827f02315665085a4d78bd187f96
dolggul2 <dolggul2@gmail.com>
2026-03-09 13:47:07 +0900

    feat: solve ps 3

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,26 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+
+def get_average(arr):
+    n = len(arr)
+    return sum(arr) // n
+
+
+def print_percentage(count, n):
+    print(f"{count / n * 100:.3f}%")
+
+
+n_case = int(input())
+for _ in range(n_case):
+    case_input = [int(str_num) for str_num in input().split(" ")]
+    n_fresher = case_input[0]
+    score_array = case_input[1:]
+    score_average = get_average(score_array)
+
+    count = 0
+    for score in score_array:
+        if score > score_average:
+            count += 1
+
+    print_percentage(count, n_fresher)
