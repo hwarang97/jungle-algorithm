a5655677b669f0a79bd1449745e8fc642b4db779
dolggul2 <dolggul2@gmail.com>
2026-03-09 11:54:05 +0900

    feat: solve ps 1

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,17 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+nums = []
+times = 9
+for _ in range(times):
+    nums.append(int(input()))
+
+maxi = nums[0]
+maxi_idx = 0
+for i, num in enumerate(nums):
+    if num > maxi:
+        maxi = num
+        maxi_idx = i
+
+print(maxi)
+print(maxi_idx + 1)
