d95ee59422b798e487fe9879399e0705cac44972
dolggul2 <dolggul2@gmail.com>
2026-03-10 11:29:22 +0900

    feat: solve ps 8

--- week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,25 @@
 # 재귀함수 - 하노이 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1914
+
+
+n = int(input())
+
+
+def get_hanoi(n):
+    return 2**n - 1
+
+
+def print_hanoi(n, source, destination, auxiliary):
+    if n == 1:
+        print(f"{source} {destination}")
+        return
+
+    print_hanoi(n - 1, source, auxiliary, destination)
+    print(f"{source} {destination}")
+    print_hanoi(n - 1, auxiliary, destination, source)
+    return
+
+
+print(get_hanoi(n))
+if n <= 20:
+    print_hanoi(n, 1, 3, 2)
