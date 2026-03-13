f3543c7e11f0395a5b874580d11ff95ce4f82348
dolggul2 <dolggul2@gmail.com>
2026-03-13 17:13:14 +0900

    feat: solve basic 89

--- week3/basic/08_linked_list.py
+++ week3/basic/08_linked_list.py
@@ -22,41 +22,53 @@
 - print_list(): 모든 노드 출력
 """
 
+
 class Node:
     """연결 리스트의 노드"""
+
     def __init__(self, data):
         self.data = data
         self.next = None
 
+
 class LinkedList:
     """단순 연결 리스트"""
+
     def __init__(self):
         self.head = None
-    
+
     def append(self, data):
         """리스트 끝에 노드 추가"""
         new_node = Node(data)
-        
+
         # TODO: 리스트가 비어있으면 head를 new_node로 설정
-        pass
-        
+        if not self.head:
+            self.head = new_node
+            return
+
         # TODO: 마지막 노드 찾기
-        pass
-        
+        current = self.head
+        while current.next:
+            current = current.next
+        last_node = current
+
         # TODO: 마지막 노드의 next를 new_node로 설정
-        pass
-    
+        last_node.next = new_node
+
     def print_list(self):
         """리스트의 모든 값 출력"""
         values = []
-        
+
         # TODO: head부터 시작
-        pass
-        
+        current = self.head
+
         # TODO: 끝까지 순회하며 값 수집
-        pass
-        
+        while current:
+            values.append(current.data)
+            current = current.next
+
         return values
+
 
 # 테스트 케이스
 if __name__ == "__main__":
@@ -69,7 +81,7 @@ if __name__ == "__main__":
     result = ll.print_list()
     print(f"리스트: {result}")
     print()
-    
+
     # 테스트 케이스 2
     print("=== 연결 리스트 테스트 2 ===")
     ll2 = LinkedList()
@@ -79,5 +91,3 @@ if __name__ == "__main__":
     ll2.append(40)
     result2 = ll2.print_list()
     print(f"리스트: {result2}")
-
-
