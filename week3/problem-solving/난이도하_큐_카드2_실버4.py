b4c49638aad1b3f19c6fb7113704eaec64f5ec7d
dolggul2 <dolggul2@gmail.com>
2026-03-16 00:01:23 +0900

    feat: solve week3 4

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,21 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+n = int(input().rstrip())
+queue = deque([num for num in range(1, n + 1)])
+
+flag = 1
+while len(queue) != 1:
+    if flag:
+        queue.popleft()
+        flag = 0
+
+    else:
+        queue.append(queue.popleft())
+        flag = 1
+
+print(queue[0])
