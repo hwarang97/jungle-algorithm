0853a4fab5f4d0836665795e85a0b85994e9bcd7
dolggul2 <dolggul2@gmail.com>
2026-03-16 16:16:50 +0900

    feat: solve week3 ps 7

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,62 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+
+import sys
+
+input = sys.stdin.readline
+times = int(input())
+stack = []
+output = []
+
+
+def process_command(command, output):
+    if command == "pop":
+        pop(output)
+
+    elif command == "size":
+        size(output)
+
+    elif command == "empty":
+        empty(output)
+
+    elif command == "top":
+        top(output)
+
+    else:
+        # push
+        push(command)
+
+
+def pop(output):
+    answer = ""
+    if stack:
+        answer = f"{stack.pop()}\n"
+    else:
+        answer = "-1\n"
+    output.append(answer)
+
+
+def size(output):
+    output.append(f"{len(stack)}\n")
+
+
+def empty(output):
+    answer = 0 if stack else 1
+    output.append(f"{answer}\n")
+
+
+def top(output):
+    answer = stack[-1] if stack else -1
+    output.append(f"{answer}\n")
+
+
+def push(command):
+    _, str_int = command.split()
+    stack.append(str_int)
+
+
+for __ in range(times):
+    command = input().rstrip()
+    process_command(command, output)
+
+sys.stdout.write("".join(output))
