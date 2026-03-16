f0aa8f208c0017390ebb7a60e1a5a8348012cec9
dolggul2 <dolggul2@gmail.com>
2026-03-16 14:21:32 +0900

    feat: solve ps 5

--- week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,21 @@
 # 이분탐색 - 수 찾기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/1920
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+n_numbers = set([int(str_int) for str_int in input().rstrip().split()])
+
+m = int(input().rstrip())
+m_numbers = [int(str_int) for str_int in input().rstrip().split()]
+
+output = []
+for number in m_numbers:
+    if number in n_numbers:
+        output.append("1\n")
+    else:
+        output.append("0\n")
+
+sys.stdout.write("".join(output))
