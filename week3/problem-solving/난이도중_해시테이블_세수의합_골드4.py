b5640354da3a77a7b192b191b54a845ff86f38fa
dolggul2 <dolggul2@gmail.com>
2026-03-16 17:54:05 +0900

    feat: solve week3 ps 9

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,26 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+numbers = [int(input().rstrip()) for _ in range(n)]
+numbers.sort()
+
+sum_set = set()
+for i in range(len(numbers)):
+    for j in range(len(numbers)):
+        sum_set.add(numbers[i] + numbers[j])
+
+
+def solution(numbers, sum_set):
+    for i in range(len(numbers) - 1, 0, -1):
+        for j in range(i - 1, -1, -1):
+            diff = numbers[i] - numbers[j]
+            if diff in sum_set:
+                return numbers[i]
+
+
+sys.stdout.write(f"{solution(numbers, sum_set)}")
