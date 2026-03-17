a999b80fde26e46be9336167acf9ec7af4eb9d5a
dolggul2 <dolggul2@gmail.com>
2026-03-17 10:52:53 +0900

    feat: solve week3 ps 11

--- week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
+++ week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
@@ -1,2 +1,31 @@
 # 이분탐색 - 두 용액 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2470
+
+import sys
+
+input = sys.stdin.readline
+
+times = int(input().rstrip())
+arr = [int(str_int) for str_int in input().rstrip().split()]
+arr.sort()
+
+
+left = 0
+right = len(arr) - 1
+smallest_diff = float("inf")
+pair = None
+while left < right:
+    current_sum = arr[right] + arr[left]
+    diff = abs(current_sum)
+    if smallest_diff > diff:
+        smallest_diff = diff
+        pair = (arr[left], arr[right])
+    if current_sum < 0:
+        left += 1
+    elif current_sum > 0:
+        right -= 1
+    else:
+        pair = (arr[left], arr[right])
+        break
+
+sys.stdout.write(f"{pair[0]} {pair[1]}")
