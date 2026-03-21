0515303802fa536ee80cc7674fda5b719a44c2e8
dolggul2 <dolggul2@gmail.com>
2026-03-21 15:14:29 +0900

    feat: solve week4 ps 2

--- week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
@@ -1,2 +1,33 @@
 # 그래프, DFS, BFS - 점프왕 쩰리 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/16173
+
+import sys
+
+
+def is_attachable(x, y, n, matrix):
+    if x >= n or y >= n or matrix[x][y] == 0:
+        return False
+
+    if matrix[x][y] == -1:
+        return True
+
+    move_delta = matrix[x][y]
+    is_left_attachable = is_attachable(x, y + move_delta, n, matrix)
+    is_right_attachable = is_attachable(x + move_delta, y, n, matrix)
+    return is_left_attachable or is_right_attachable
+
+
+input = sys.stdin.readline
+n = int(input().rstrip())
+matrix = [
+    [move_amount for move_amount in map(int, input().rstrip().split())]
+    for _ in range(n)
+]
+
+output = ""
+if is_attachable(0, 0, n, matrix):
+    output = "HaruHaru"
+else:
+    output = "Hing"
+
+sys.stdout.write(output)
