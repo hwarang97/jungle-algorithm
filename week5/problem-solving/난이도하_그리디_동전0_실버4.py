6f57e3eaed50365b1e27c736fe78b204048a442e
dolggul2 <dolggul2@gmail.com>
2026-03-27 17:32:40 +0900

    feat: solve week5 ps 2

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,27 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+import sys
+
+
+def make_wallet(n):
+    """
+    동전 10개를 가진 지갑을 만들어 반환하는 함수
+    """
+    wallet = [int(input().rstrip()) for _ in range(n)]
+    return wallet
+
+
+n, change = list(map(int, input().rstrip().split()))
+wallet = make_wallet(n)
+total = 0
+
+for i in range(n - 1, -1, -1):
+    if change == 0:
+        break
+
+    div, remain = divmod(change, wallet[i])
+    total += div
+    change = remain
+
+sys.stdout.write(f"{total}")
