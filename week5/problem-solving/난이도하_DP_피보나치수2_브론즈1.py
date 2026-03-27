dd1cb7c9f90445e02aed956d4a196865239e5471
dolggul2 <dolggul2@gmail.com>
2026-03-27 17:40:01 +0900

    feat: sovle week5 ps 3 iteration

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -4,25 +4,41 @@
 import sys
 
 input = sys.stdin.readline
-MAX_N = 90
+
+
+# def fibonacci(n, dp):
+#     if n == 0:
+#         dp[0] = 0
+#         return 0
+
+#     if n == 1:
+#         dp[1] = 1
+#         return 1
+
+#     if dp[n] != -1:
+#         return dp[n]
+
+#     dp[n] = fibonacci(n - 2, dp) + fibonacci(n - 1, dp)
+#     return dp[n]
 
 
 def fibonacci(n, dp):
     if n == 0:
-        dp[0] = 0
         return 0
 
     if n == 1:
-        dp[1] = 1
         return 1
 
-    if dp[n] != -1:
-        return dp[n]
+    dp[0] = 0
+    dp[1] = 1
+    i = 2
+    while i <= n:
+        dp[i] = dp[i - 1] + dp[i - 2]
+        i += 1
 
-    dp[n] = fibonacci(n - 2, dp) + fibonacci(n - 1, dp)
     return dp[n]
 
 
 n = int(input().rstrip())
-dp = [-1] * (MAX_N + 1)
+dp = [-1] * (n + 1)
 sys.stdout.write(f"{fibonacci(n, dp)}")
