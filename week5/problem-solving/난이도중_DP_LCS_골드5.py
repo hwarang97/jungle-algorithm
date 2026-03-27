49c2218097800e7512007fcf0725ce2692a8554a
dolggul2 <dolggul2@gmail.com>
2026-03-27 17:00:12 +0900

    feat: solve week5 ps 1

--- week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -1,2 +1,33 @@
 # DP - LCS (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9251
+
+import sys
+
+input = sys.stdin.readline
+
+
+def digit_to_int(num_str):
+    """
+    5자리 10진수를 정수로 변환하는 함수
+    args:
+        x: 0으로 시작할 수 있는 5자리 문자열
+    return:
+        x와 같은 값을 갖는 정수
+    """
+    return int(num_str)
+
+
+expression = input().strip()
+total = 0
+sign = 1
+minus_position = -1
+for i in range(len(expression)):
+    if expression[i] == "-":
+        total += sign * sum(
+            map(digit_to_int, expression[minus_position + 1 : i].split("+"))
+        )
+        sign = -1
+        minus_position = i
+
+total += sign * sum(map(digit_to_int, expression[minus_position + 1 :].split("+")))
+print(total)
