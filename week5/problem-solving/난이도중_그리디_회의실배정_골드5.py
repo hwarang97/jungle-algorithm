ed91e0a3c11c5950db531098c15fc555bcf5d8da
dolggul2 <dolggul2@gmail.com>
2026-03-28 09:47:12 +0900

    feat: solve week5 ps 5

--- week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
+++ week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
@@ -1,2 +1,22 @@
 # 그리디 - 회의실 배정 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1931
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+arr = [list(map(int, input().rstrip().split())) for _ in range(n)]
+
+arr.sort(key=lambda x: (x[1], x[0]))
+
+prev = arr[0]
+output = [prev]
+for i in range(1, len(arr)):
+    meeting = arr[i]
+    if prev[1] <= meeting[0]:
+        print(f"{prev=} {meeting=}")
+        output.append(meeting)
+        prev = meeting
+
+sys.stdout.write(f"{len(output)}")
