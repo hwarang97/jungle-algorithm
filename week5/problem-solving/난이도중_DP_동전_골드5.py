95679b7c9c59ccebc5f8304a1b96fbeeae3cebfc
dolggul2 <dolggul2@gmail.com>
2026-03-30 11:42:59 +0900

    feat: solve week5 ps 8

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,48 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+
+import sys
+
+input = sys.stdin.readline
+
+
+def count_combination():
+    """
+    주어진 동전들로 잔액을 처리하는 경우수를 반환하는 함수
+    """
+    num_coins = int(input())
+    coins = list(map(int, input().split()))
+    change = int(input())
+    memo = [dict() for _ in range(num_coins)]
+
+    return helper(num_coins - 1, coins, change, memo)
+
+
+def helper(i, coins, change, memo):
+    """
+    i번째 이하 동전들만 사용해 잔액을 처리하는 경우수를 반환하는 함수
+    """
+    if change in memo[i]:
+        return memo[i][change]
+
+    if i == 0:
+        if change % coins[i] == 0:
+            memo[i][change] = 1
+        else:
+            memo[i][change] = 0
+        return memo[i][change]
+
+    k = change // coins[i]
+    memo[i][change] = 0
+    for j in range(k + 1):
+        memo[i][change] += helper(i - 1, coins, change - (j * coins[i]), memo)
+
+    return memo[i][change]
+
+
+test_time = int(input())
+output = []
+for _ in range(test_time):
+    output.append(str(count_combination()))
+
+print("\n".join(output))
